91bbf4a91e069a600ef92b670a2206f7d5227f5b
Jack Marchant <jack@expert360.com>
2018-08-23 20:37:05 +1000

    add archives

--- mix.exs
+++ mix.exs
@@ -1,16 +1,19 @@
 defmodule ExDocker.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :ex_docker,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       escript: escript(),
       description: description(),
       package: package(),
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -39,6 +42,20 @@ defmodule ExDocker.MixProject do
       main_module: ExDocker.Cli,
       name: :exdocker
     ]
+  end
+
+  def aliases do
+    [
+      build: &build_releases/1
+    ]
+  end
+
+  defp build_releases(_) do
+    Mix.Tasks.Compile.run([])
+    Mix.Tasks.Archive.Build.run([])
+    Mix.Tasks.Archive.Build.run(["--output=exdocker.ez"])
+    File.rename("exdocker.ez", "./archives/exdocker.ez")
+    File.rename("exdocker-#{@version}.ez", "./archives/exdocker-#{@version}.ez")
   end
 
   def description do
